695741edd10143acb104e245f71d61c049834dac
Steven Klimek <steven.klimek7@yahoo.com>
2024-05-27 21:08:41 -0500

    final

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -31,13 +31,21 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+    setPosts(posts.map(post => {
+      if (post.id !== postId) return post
+      let updatedPost = {...post, likes: post.likes + 1}
+      return updatedPost
+      }
+  )
+)
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts likePost={likePost} posts={posts}/>
+      <Posts likePost={likePost} posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Post from './Post';
+import LikeSection from './LikeSection';
+import PostHeader from './PostHeader';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
@@ -9,6 +11,9 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(pst => <Post likePost={likePost} post={pst} key={pst.id} />)
+      }
     </div>
   );
 };
